3fac204af4262a91797b67653ec59bedf7a9730d
AMALGSOFT <rivangushu200241>
2023-08-28 19:06:07 +0300

    first commt

--- djangoWebsite/settings.py
+++ djangoWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-*evc8a4yo$)y!x0zutfn!t+n3%vjf1tr2zhi!*+km)f_qg@$1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.heroku.app','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),]
